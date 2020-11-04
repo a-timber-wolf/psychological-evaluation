bbe03b9071f1b8cc105b5cd995a1d118606e0f8f
wolf <18803547489@163.com>
2020-11-04 17:49:48 +0800

    wolf

---
+++ src/main/java/com/example/psychologicalassessmentsystem/PsychologicalAssessmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.psychologicalassessmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologicalAssessmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsychologicalAssessmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/psychologicalassessmentsystem/PsychologicalAssessmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.psychologicalassessmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychologicalAssessmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
