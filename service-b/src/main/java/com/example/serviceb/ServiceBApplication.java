3c8082e607ad9db25ba5f69239dbc643a692c887
wolf <18803547489@163.com>
2020-11-05 14:39:22 +0800

    wolf

--- service-b/src/main/java/com/example/serviceb/ServiceBApplication.java
+++ service-b/src/main/java/com/example/serviceb/ServiceBApplication.java
@@ -2,8 +2,14 @@ package com.example.serviceb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @author EMD-WANGENGUI
+ * @date 2020/11/05 14:34
+ */
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceBApplication {
 
 	public static void main(String[] args) {
