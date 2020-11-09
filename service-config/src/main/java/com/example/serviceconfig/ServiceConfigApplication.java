c3bfd017a63f09396805345a9e9f1e353da321a9
wolf <18803547489@163.com>
2020-11-09 17:51:37 +0800

    spring cloud config

--- service-config/src/main/java/com/example/serviceconfig/ServiceConfigApplication.java
+++ service-config/src/main/java/com/example/serviceconfig/ServiceConfigApplication.java
@@ -1,5 +1,6 @@
 package com.example.serviceconfig;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -9,9 +10,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @author EMD-WANGENGUI
  * @date 2020/11/09 14:23
  */
-@SpringBootApplication
 @EnableEurekaClient
 @EnableConfigServer
+@SpringBootApplication
 public class ServiceConfigApplication {
 
 	public static void main(String[] args) {
